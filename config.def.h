c7b00df56b2c2fe14cb309e07d2ae0580fc1345c
SupremMotu <francisd132435@gmail.com>
2021-01-07 15:16:46 -0800

    tweak color and font

--- config.def.h
+++ config.def.h
@@ -11,14 +11,14 @@ static const unsigned int systrayspacing = 2;   /* systray spacing */
 static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
 static const int showsystray             = 1;   /* 0 means no systray */
 static const char font[]            = "Hack 10.5";
-static const char dmenufont[]       = "Noto Sans:size=9";
+static const char dmenufont[]       = "Noto Sans-9";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#61A0FF";
 static const char col_urgborder[]   = "#FF7F7F";
-static const unsigned int baralpha = 220;
+static const unsigned int baralpha = 195;
 static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
